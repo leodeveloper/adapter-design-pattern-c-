89fd187e62f5e5ad79e02ce700bc2b9e770918c2
Muhammad Suleman <leodeveloper@gmail.com>
2018-10-12 19:30:19 +0100

    First commit

---
+++ AdaptorDesignPattern/Export.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaptorDesignPattern
+{
+    public abstract class Export
+    {
+        public abstract void Save();
+    }
+
+    public class Excel : Export
+    {
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Pdf : Export
+    {
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Csv : Export
+    {
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ThirdPartyExport : Export
+    {
+        public override void Save()
+        {
+            ThirdParty thirdParty = new ThirdParty();
+            thirdParty.Export();
+        }
+    }
+
+
+    public class ThirdParty
+    {
+        public void Export()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AdaptorDesignPattern/Program.cs
+++ AdaptorDesignPattern/Program.cs
@@ -10,6 +10,17 @@ namespace AdaptorDesignPattern
     {
         static void Main(string[] args)
         {
+            Export export = new Excel();
+            export.Save();
+
+            export = new Pdf();
+            export.Save();
+
+            export = new Csv();
+            export.Save();
+            //Make notcompitable class to compitable, ThirdPart liberary might does not have Save method now here the adaptor pattern comes, please check file Export.cs class name "ThirdPartyExport"
+            export = new ThirdPartyExport();
+            export.Save();
         }
     }
 }
